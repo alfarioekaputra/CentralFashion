5eb1aac9bc468688b27f290396958cfbdf9be4fb
Rio <alfarioekaputra@gmail.com>
2025-01-29 22:17:17 +0700

    Fix: Remove slash from route beginning with a '/'

--- master/suppliers/urls.py
+++ master/suppliers/urls.py
@@ -4,7 +4,7 @@ from .views import index, create, export_to_excel, load_filter_form, clear_filte
 
 urlpatterns = [
   path("", index, name="supplier_index"),
-  path("/create", create, name="supplier_create"),
+  path("create", create, name="supplier_create"),
   path("export-excel/", export_to_excel, name="supplier_export_excel"),
   path('supplier/load-filter-form/', load_filter_form, name='supplier_load_filter_form'),
   path('supplier/clear-filter/', clear_filter, name='supplier_clear_filter'),
